6c227cde49d5f63a63322419b5c1c74450f05563
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2023-07-13 15:56:51 -0300

    chore: adding husky and lint-staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+/**
+ * Renders the home page component.
+ *
+ * @return {JSX.Element} The rendered home page component.
+ */
 export default function Home() {
-  /**
-   * Renders the Home component.
-   *
-   * @return {JSX.Element} The rendered Home component.
-   */
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Next 13 - Template</h1>
